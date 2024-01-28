cf93d11c7a7e582b5e02ae61c04403b9424b90a2
zeng <hygs12345@163.com>
2024-01-28 15:45:47 +0800

    Site updated: 2024-01-28 15:45:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=[];function toRandomPost(){
+var posts=["2024/01/28/nlp比赛中trick的使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
