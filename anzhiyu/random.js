e207700a92f7f79bee881f1f4da1214cc6382328
zeng <hygs12345@163.com>
2024-03-17 19:43:26 +0800

    Site updated: 2024-03-17 19:43:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/29/机器学习中你可能需要的数学基础/","2024/03/04/模式识别笔记/","2024/01/28/nlp比赛中trick的使用/"];function toRandomPost(){
+var posts=["2024/01/29/机器学习中你可能需要的数学基础/","2024/01/28/nlp比赛中trick的使用/","2024/03/04/统计决策方法/","2024/03/17/概率密度估计/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
